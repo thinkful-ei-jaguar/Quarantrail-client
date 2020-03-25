da6b8ad9f4fe72c9b29059d13dbcef05bde74fa1
Alex <robert.perez6@g.austincc.edu>
2020-03-25 12:50:50 -0500

    added componentdidmount ingmaepage

--- src/Routes/Homepage/HomePage.js
+++ src/Routes/Homepage/HomePage.js
@@ -1,31 +1,26 @@
-import React, { Component } from "react";
-import BooleanContext from "../../Context/BooleanContext";
-import LandingPage from "../LandingPage/LandingPage";
-import StartPage from "../StartPage/StartPage";
-import GamePage from "../GamePage/GamePage";
-import gameService from "../../services/gameService";
+import React, { Component } from 'react'
+import PersonContext from '../../Context/PersonContext'
+import LandingPage from '../LandingPage/LandingPage'
+import StartPage from '../StartPage/StartPage'
+import GamePage from '../GamePage/GamePage'
+import gameService from '../../services/gameService'
 
 //import './HomePage.css'
 
+
+
 export default class HomePage extends Component {
-  static contextType = BooleanContext;
-  componentDidMount() {
-    gameService
-      .getGameinfo()
-      .then(info => {
-        console.log(info);
-        this.context.setPersonInfo(info);
-        console.log(this.context.starter);
-      })
-      .catch(this.context.setError);
-  }
-  render() {
-    return (
-      <div className="homePage">
-        {this.context.start && <LandingPage />}
-        {this.context.userPage && <StartPage />}
-        {this.context.game && <GamePage />}
-      </div>
-    );
-  }
+    static contextType = PersonContext
+    
+    render() {
+        return (
+            <div className='homePage'>
+                
+                {this.context.start && <LandingPage />}
+                {this.context.userPage && <StartPage />}
+                {this.context.game ? <GamePage/> : null}
+
+            </div>
+        )
+    }
 }
