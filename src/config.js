5cb74b8f98e4f4804c8129be56ba97f7e734b2d5
Alex <robert.perez6@g.austincc.edu>
2020-03-26 13:52:36 -0500

    deployed server on config

--- src/config.js
+++ src/config.js
@@ -1,3 +1,5 @@
 export default {
-    API_ENDPOINT: 'http://localhost:8000/api',
+    API_ENDPOINT: 'https://infinite-mesa-74719.herokuapp.com/api',
 }
+//https://infinite-mesa-74719.herokuapp.com/api
+//http://localhost:8000/api
