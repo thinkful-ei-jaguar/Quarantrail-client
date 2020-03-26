984178ec4eef9712391f6b0705b477e13e027a66
Taylor Piccarreto <tpic12@gmail.com>
2020-03-26 13:07:55 -0400

    market/taylor

--- src/Components/Activities/Activities.js
+++ src/Components/Activities/Activities.js
@@ -7,16 +7,26 @@ export default class Activities extends Component {
     constructor(props) {
         super(props);
         this.state = {
+          options: {
+            first: 1,
+            second: 2,
+            third: 3
+          }
         };
       }
 
 
     render() {
+      // const options = {
+      //   first: this.props.first,
+      //   second: this.props.second,
+      //   third: this.props.third
+      // }
         return <div class="none">
-            <h1>Activities:</h1>
-            <button class="mybutton">1</button>
-            <button class="mybutton">2</button>
-            <button class="mybutton">3</button>
+        <h1>Activities:</h1>
+          <button class="mybutton">{this.state.options.first}</button>
+          <button class="mybutton">{this.state.options.second}</button>
+          <button class="mybutton">{this.state.options.third}</button>
         </div>
       }
 }
---
+++ src/Components/Store/Store.js
@@ -0,0 +1,67 @@
+import React, { Component } from "react";
+import PersonContext from "../../Context/PersonContext";
+import "./Store.css"
+
+export default class Stock extends Component {
+    static contextType = PersonContext;
+
+    state ={
+      foodCounter: 0,
+      toiletPaperCounter: 0
+    }
+
+    handlePlusFood = () => {
+      if(this.state.foodCounter === 6) {
+        return;
+      }
+      this.setState({foodCounter: this.state.foodCounter+1})
+
+    }
+
+    handleMinusFood = () => {
+      if(this.state.foodCounter === 0){
+        return;
+      }
+      this.setState({foodCounter: this.state.foodCounter-1})
+    }
+
+    handlePlusTp = () => {
+      if(this.state.toiletPaperCounter === 6) {
+        return;
+      }
+      this.setState({toiletPaperCounter: this.state.toiletPaperCounter+1})
+    }
+
+    handleMinusTp = () => {
+      if(this.state.toiletPaperCounter === 0) {
+        return;
+      }
+      this.setState({toiletPaperCounter: this.state.toiletPaperCounter-1})
+    }
+
+    handleCheckout = () => {
+      const {foodCounter, toiletPaperCounter} = this.state
+      console.log(`You bought ${foodCounter} cups of noodles and ${toiletPaperCounter} rolls of toilet paper!`)
+    }
+
+    render() {
+      const {foodCounter, toiletPaperCounter } = this.state
+        return(
+        <div className="store">
+            <p>Welcome to Trader Jone's! As you see were a little bare right now due to the crisis, but hey all business is good business right? Look around and let me know if you'd like any food or toilet paper.</p>
+            <div className='store-food'>
+              <p>Food</p>
+              {foodCounter}
+              <button onClick={this.handlePlusFood}>+</button>
+              <button onClick={this.handleMinusFood}>-</button>
+            </div>
+            <div className='store-toilet-paper'>
+              <p>Toilet Paper</p>
+              {toiletPaperCounter}
+              <button onClick={this.handlePlusTp}>+</button>
+              <button onClick={this.handleMinusTp}>-</button>
+            </div>
+            <button className='checkout-button' onClick={this.handleCheckout}>Buy</button>
+        </div>
+      )}
+}
