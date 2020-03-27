a7dddb3a1cf80aecfe9ea35a7938a45ec4d2b2c1
Nghi Tran <nghi.t.tran8@gmail.com>
2020-03-27 12:47:05 -0700

    added container for status

--- src/Routes/GamePage/GamePage.js
+++ src/Routes/GamePage/GamePage.js
@@ -5,33 +5,32 @@ import Activities from "../../Components/Activities/Activities";
 import PersonContext from "../../Context/PersonContext";
 import "./GamePage.css";
 import StatusBar from "../../Components/StatusBar/StatusBar";
-import FirstDay from "../../Components/FirstDay/FirstDay"
+import FirstDay from "../../Components/FirstDay/FirstDay";
 import Day from "../../Components/Day/Day";
-import gameService from '../../services/gameService'
-
-
+import gameService from "../../services/gameService";
 
 export default class GamePage extends Component {
   static contextType = PersonContext;
-  componentDidMount(){
-    if(this.context.day===0){
-      gameService.getGameinfo()
-      .then(info=>{
+  componentDidMount() {
+    if (this.context.day === 0) {
+      gameService
+        .getGameinfo()
+        .then(info => {
           this.context.setPersonInfo(info);
-          console.log(this.context)
-      }) 
-      .catch(this.context.setError)
-    }  
+          console.log(this.context);
+        })
+        .catch(this.context.setError);
+    }
   }
-
-
 
   render() {
     return (
       <section className="gamePage">
-        {this.context.day===0?<FirstDay/>:<></>}
-        <StatusBar/>
-        <Day/>
+        {this.context.day === 0 ? <FirstDay /> : <></>}
+        <div className="top">
+          <StatusBar />
+          <Day />
+        </div>
         <Stock />
         <Activities />
         <Link to="/market">
