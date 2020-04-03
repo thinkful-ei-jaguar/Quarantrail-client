103b5bc8def274c2d6aba8b5de69a9d4dc86e85d
Nghi Tran <nghi.t.tran8@gmail.com>
2020-04-03 15:31:54 -0700

    added new logo and css for landing page

--- src/Components/Instruction/Instruction.js
+++ src/Components/Instruction/Instruction.js
@@ -1,15 +1,20 @@
 import React from "react";
-import './Instruction.css'
+import "./Instruction.css";
 
 function Instruction() {
-  return( 
-  <section className="instruction-section">
-    <div className='instruction'>
-      <h2>The year was 2020...</h2>
-      <p>...in a world so similar to ours it was pretty much the same world, a global pandemic swept across the planet and forced everyone to stay inside to avoid contagion. Sounds easy enough you say? maybe on day 1 or 2, but a whole week? a whole month? How long can you last, and is it worth it to go out to the market for your sanity? We'll find out as you journey through boredom and risk in Corona Trail.</p>
-      <br />
-      <p>You have two gauges: boredom and chance of infection, if either of these get too high you lose. It is your job to balance these to make sure your character survives. Boredom can be reduced by doing activities but with every activity you do you risk being infected. You'll also need to keep track of, and maintain, your supply of food and toilet paper, but you may not want to get too much...</p>
-    </div>
-  </section>
-)}
+  return (
+    <section className="instruction-section">
+      <div className="instruction">
+        <p>
+          You have two gauges: boredom and chance of infection, if either of
+          these get too high you lose. It is your job to balance these to make
+          sure your character survives. Boredom can be reduced by doing
+          activities but with every activity you do you risk being infected.
+          You'll also need to keep track of, and maintain, your supply of food
+          and toilet paper, but you may not want to get too much...
+        </p>
+      </div>
+    </section>
+  );
+}
 export default Instruction;
--- src/Routes/LandingPage/LandingPage.js
+++ src/Routes/LandingPage/LandingPage.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import BooleanContext from "../../Context/BooleanContext";
 import "./LandingPage.css";
-import Virus from "../../Images/virus.png";
+import Logo from "../../Images/logo.svg";
 import Music from "../../Components/Music/Music";
 import Song from "../../Sound/8bitmenu.mp3";
 
@@ -10,12 +10,21 @@ export default class LandingPage extends Component {
 
   render() {
     return (
-      <div className="landingpage">
-        <img src={Virus} alt="virus logo" />
-        <h1>Corona Trail</h1>
-        <p>Covid-19</p>
+      <div className="landingPage">
+        <img src={Logo} alt="quaran trail - stay home" />
+        <div className="intro">
+          <h1>The year was 2020...</h1>
+          <p>
+            ...in a world so similar to ours it was pretty much the same world,
+            a global pandemic swept across the planet and forced everyone to be
+            put in quarantine. It may be easy on day 1 or 2, but a whole week? a
+            whole month? How long can you last, and is it worth go out to the
+            market for your sanity? We'll find out as you journey through
+            boredom and risk in QuaranTrail.
+          </p>
+        </div>
         <button onClick={this.context.renderUser}>Start</button>
-        <p className="player">1 Player</p>
+        {/* <p className="player">1 Player</p> */}
         <Music song={Song} />
       </div>
     );
