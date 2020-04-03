5f5ea32212cbe8125006eec7186773f9459ac3b0
Taylor Piccarreto <tpic12@gmail.com>
2020-04-03 12:14:58 -0400

    started testing

---
+++ src/Components/App/app.spec.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import toJson from 'enzyme-to-json'
+import App from './App'
+
+describe(`App component`, () => {
+
+  it('renders by default', () => {
+    const wrapper = shallow(<App />)
+    expect(toJson(wrapper)).toMatchSnapshot()
+  })
+})
